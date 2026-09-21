29d94e0ca58c8ffcde4a8e8dd0f75fcc9ece9709
Ofigel86 <michaofigel@gmail.com>
2026-09-21 21:04:36 +0300

    Delete Counter-Strike Source v34 directory

--- Counter-Strike Source v34/Feature.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "Accuracy.hpp"
-#include "Aimbot.hpp"
-#include "Triggerbot.hpp"
-#include "DataManager.hpp"
-#include "PlayerList.hpp"
-#include "Menu.hpp"
-#include "Render.hpp"
--- Counter-Strike Source v34/Sounds.hpp
+++
@@ -1,3 +0,0 @@
-extern char rawData[3936];
-extern char rawData1[41620];
-extern char rawData2[286948];
