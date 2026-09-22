79d6139cb8ee727e358df55af04e9da94e7d86c6
Ofigel86 <177324285+Ofigel86@users.noreply.github.com>
2026-09-22 07:20:11 +0000

    update CL_Move from Segregation with SDK check
    Co-authored-by: arena-agent <297053741+arena-agent@users.noreply.github.com>

--- Counter-Strike Source v34/CL_Move.cpp
+++ Counter-Strike Source v34/CL_Move.cpp
@@ -1,23 +1,36 @@
 #include "Main.h"
 #include "detours.h"
 
-typedef void ( *CL_Move_t )( float, bool );
+typedef void (*CL_Move_t)(float,bool);
 CL_Move_t _CL_Move;
 
-void Hooked_CL_Move( float accumulated_extra_samples, bool bFinalTick )
+void Hooked_CL_Move(float accumulated_extra_samples,bool bFinalTick)
 {
-	_CL_Move( accumulated_extra_samples, bFinalTick );
-
-	if( g_CVars.Miscellaneous.Speedhack && GetAsyncKeyState( 0x45 ) )
-	{
-		g_bCL_Move = true;
-		for( int i = 0; i < g_CVars.Miscellaneous.SpeedhackValue; i++ ) _CL_Move( accumulated_extra_samples, bFinalTick );
-	}
-
-	g_bCL_Move = false;
+    _CL_Move(accumulated_extra_samples,bFinalTick);
+    if(bFinalTick && g_CVars.Miscellaneous.Speedhack && (GetAsyncKeyState(0x45) & 0x8000))
+    {
+        if(g_pEngineClient && g_pEngineClient->IsInGame())
+        {
+            BasePlayer* LocalPlayer=(BasePlayer*)g_pClientEntityList->GetClientEntity(g_pEngineClient->GetLocalPlayer());
+            if(LocalPlayer && LocalPlayer->m_lifeState()==0)
+            {
+                g_bCL_Move=true;
+                for(int i=0;i<g_CVars.Miscellaneous.SpeedhackValue;i++) _CL_Move(accumulated_extra_samples,bFinalTick);
+                g_bCL_Move=false;
+                return;
+            }
+        }
+        g_bCL_Move=true;
+        for(int i=0;i<g_CVars.Miscellaneous.SpeedhackValue;i++) _CL_Move(accumulated_extra_samples,bFinalTick);
+        g_bCL_Move=false;
+    }
+    else
+    {
+        g_bCL_Move=false;
+    }
 }
 
-void CL_Move( void )
+void CL_Move(void)
 {
-	_CL_Move = ( CL_Move_t ) DetourFunction( ( PBYTE ) ( ( DWORD ) BASE_ENGINE + 0x42510 ), ( PBYTE ) Hooked_CL_Move );
+    _CL_Move=(CL_Move_t)DetourFunction((PBYTE)((DWORD)BASE_ENGINE+0x42510),(PBYTE)Hooked_CL_Move);
 }
